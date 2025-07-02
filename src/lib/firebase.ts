6666ef25325a3d9fb0bb95a6187e4639592bf754
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2025-07-02 20:13:02 -0300

    build: configurar a biblioteca do firebase usando variaveis de ambiente

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore"; // Importe o Firestore
 
 // A configuração do Firebase do seu app web (COPIE DO SEU CONSOLE DO FIREBASE)
 const firebaseConfig = {
-  apiKey: "AIzaSyA3BA1XCTjk3gPFP9xgUk14ke8UMc0Rn9s",
-  authDomain: "sarca-ardente.firebaseapp.com",
-  projectId: "sarca-ardente",
-  storageBucket: "sarca-ardente.firebasestorage.app",
-  messagingSenderId: "540488094736",
-  appId: "1:540488094736:web:4e124ebf75c22ff2732695"
+  apiKey: process.env.NExT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Inicialize o Firebase de forma segura (evita reinicializar no Next.js)
