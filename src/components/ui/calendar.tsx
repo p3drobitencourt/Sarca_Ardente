291444c0026c913b49c7354eece5261fba9065f8
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2025-07-22 00:18:43 -0300

    fix: algumas correções nos inputs de calendários

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,21 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker, DropdownProps } from "react-day-picker";
+import { ptBR } from "date-fns/locale";
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
   className,
@@ -25,13 +26,15 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      locale={ptBR}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -62,42 +65,71 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-        // A SOLUÇÃO: Usar o componente Select do shadcn para os dropdowns
-        Dropdown: ({ value, onChange, children, ...props }) => {
+        Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
           const options = React.Children.toArray(
             children
-          ) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[]
-          const selected = options.find((child) => child.props.value === value)
+          ) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[];
+          const selected = options.find((child) => child.props.value === value);
+
           const handleChange = (value: string) => {
             const changeEvent = {
               target: { value },
-            } as React.ChangeEvent<HTMLSelectElement>
-            onChange?.(changeEvent)
-          }
-          return (
-            <Select
-              value={value?.toString()}
-              onValueChange={(value) => {
-                handleChange(value)
-              }}
-            >
-              <SelectTrigger className="pr-1.5 focus:ring-0">
-                <SelectValue>{selected?.props?.children}</SelectValue>
-              </SelectTrigger>
-              <SelectContent position="popper">
-                <div className="max-h-48 overflow-y-auto">
-                    {options.map((option, id: number) => (
+            } as React.ChangeEvent<HTMLSelectElement>;
+            onChange?.(changeEvent);
+          };
+
+          if (props.name === "months") {
+            return (
+              <Select
+                value={value?.toString()}
+                onValueChange={(value) => {
+                  handleChange(value);
+                }}
+              >
+                <SelectTrigger className="pr-1.5 focus:ring-0 w-[120px]">
+                  <SelectValue>{selected?.props?.children}</SelectValue>
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {options.map((option, id: number) => (
                     <SelectItem
+                      key={`${option.props.value}-${id}`}
+                      value={option.props.value?.toString() ?? ""}
+                    >
+                      {option.props.children}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            );
+          }
+          
+          if (props.name === "years") {
+            return (
+              <Select
+                value={value?.toString()}
+                onValueChange={(value) => {
+                  handleChange(value);
+                }}
+              >
+                <SelectTrigger className="pr-1.5 focus:ring-0 w-[80px]">
+                  <SelectValue>{selected?.props?.children}</SelectValue>
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <ScrollArea className="h-48">
+                    {options.map((option, id: number) => (
+                      <SelectItem
                         key={`${option.props.value}-${id}`}
                         value={option.props.value?.toString() ?? ""}
-                    >
+                      >
                         {option.props.children}
-                    </SelectItem>
+                      </SelectItem>
                     ))}
-                </div>
-              </SelectContent>
-            </Select>
-          )
+                  </ScrollArea>
+                </SelectContent>
+              </Select>
+            );
+          }
+          return null
         },
       }}
       captionLayout="dropdown-buttons"
@@ -105,8 +137,8 @@ function Calendar({
       toYear={new Date().getFullYear()}
       {...props}
     />
-  )
+  );
 }
-Calendar.displayName = "Calendar"
+Calendar.displayName = "Calendar";
 
-export { Calendar }
+export { Calendar };
